2fc29d0418ce926dccd72e36e4076d281504fc94
iitzex <iitzex@gmail.com>
2019-12-24 20:49:41 +0800

    update arrival time

--- fr24.py
+++ fr24.py
@@ -2,24 +2,27 @@ import time
 
 import requests
 
-import ujson
-
 headers = {
     'Host': 'data-live.flightradar24.com',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:55.0) Gecko/20100101 Firefox/55.0',
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Referer': 'https://www.flightradar24.com/24.61,121.52/8',
+    'Referer': 'https://www.flightradar24.com/24.61,121.52/3',
     'Origin': 'https://www.flightradar24.com',
     'DNT': '1',
     'Connection': 'keep-alive',
 }
 
+# https://data-live.flightradar24.com/zones/fcgi/feed.js?
+# bounds=44.77,14.02,99.12,163.28&
+# faa=0&satellite=1&mlat=0&flarm=0&adsb=1&gnd=0&air=1&vehicles=0&estimated=1&maxage=14400&gliders=0&stats=1
+# &enc=qPgPtpctGA0YZPTS9NF_mD6QVwq27mkhfOawf2qaSh4&to=TPE
 
-def area(bound):
+
+def area(bounds):
     params = (
-        ('bounds', bound),
-        ('faa', '1'),
+        ('bounds', bounds),
+        ('faa', '0'),
         ('mlat', '1'),
         ('flarm', '1'),
         ('adsb', '1'),
@@ -30,6 +33,8 @@ def area(bound):
         ('maxage', '14400'),
         ('gliders', '1'),
         ('stats', '1'),
+        ('to', 'TPE'),
+
     )
     r = requests.get('https://data-live.flightradar24.com/zones/fcgi/feed.js',
                      headers=headers, params=params)
@@ -38,6 +43,43 @@ def area(bound):
     j = r.json()
     # print(j)
     return j
+
+
+def detail(bounds, ix):
+    params = (
+        ('bounds', bounds),
+        ('faa', '1'),
+        ('satellite', '1'),
+        ('mlat', '1'),
+        ('flarm', '1'),
+        ('adsb', '1'),
+        ('gnd', '1'),
+        ('air', '1'),
+        ('vehicles', '0'),
+        ('estimated', '1'),
+        ('maxage', '14400'),
+        ('gliders', '0'),
+        ('stats', '1'),
+        ('selected', ix),
+        ('ems', '1'),
+        ('enc', 'qPgPtpctGA0YZPTS9NF_mD6QVwq27mkhfOawf2qaSh4'),
+        ('to', 'TPE'),
+    )
+
+    r = requests.get(
+        'https://data-live.flightradar24.com/zones/fcgi/feed.js', headers=headers, params=params)
+
+    try:
+        j = r.json()
+        print(j)
+        # ems = j['selected-aircraft']['ems']
+        # alt = ems['AGPS']
+        # wind = ems['WIND']
+
+        # return alt, wind
+
+    except KeyError as e:
+        pass
 
 
 def arr_time(ix):
@@ -57,12 +99,17 @@ def arr_time(ix):
         #     print(k, v)
         #     print('--')
         return arr
-
-
     except KeyError as e:
         pass
 
 
 if __name__ == '__main__':
-    ix = '1cdc722e'
-    arr_time(ix)
+    # ix = '1cdc722e'
+    # arr_time(ix)
+
+    bounds = '71.81,-35.85,21.19,-82.17'
+    # j = area(bounds)
+    # print(j)
+    ix = '234bcb93'
+    r = detail(bounds, ix)
+    print(r)
--- main.py
+++ main.py
@@ -8,54 +8,50 @@ import fr24
 
 
 def main():
-    total = 0
     TPE = {}
     try:
-        bound = '40.00,18.50,100.00,144.60'
-        north = fr24.area(bound)
-        bound = '26.20,1.50,100.00,144.60'
-        south = fr24.area(bound)
-        j = {**north, **south}
+        bounds = '71.81,-35.85,21.19,-82.17'
+        j = fr24.area(bounds)
+        # j = {**north, **south}
         print(len(j))
-        
+
         for k, v in j.items():
             if k == 'full_count' or k == 'version' or k == 'stats' or k == 'visible' or k == 'selected-aircraft':
                 continue
 
             level = int(v[4])
-            dst = v[12]
             if level == 0:
                 continue
 
-            if dst == 'TPE':
-                lat = float(v[1])
-                lon = float(v[2])
-                level = int(v[4])
-                src = v[11]
-                callsign = v[16]
+            dst = v[12]
+            lat = float(v[1])
+            lon = float(v[2])
+            # level = int(v[4])
+            src = v[11]
+            callsign = v[16]
 
-                total += 1
-                ts = fr24.arr_time(k)
-                if ts:
-                    t = {callsign: int(ts)}
-                    print(t)
-                    TPE.update(t)
-                    # print('.', end='')
+            ts = fr24.arr_time(k)
+            if ts:
+                t = {callsign: [int(ts), level]}
+                TPE.update(t)
+                # print(t)
+                print('.', end='', flush=True)
+        print('')
 
-        print(total)
         with open('log', 'w') as f:
-            TPE = OrderedDict(sorted(TPE.items(), key=itemgetter(1)))
+            TPE = OrderedDict(
+                sorted(TPE.items(), key=itemgetter(1), reverse=True))
+
             for k, v in TPE.items():
-                arr = datetime.fromtimestamp(v)
-                print("%8s %02d:%02d" % (k, arr.hour, arr.minute))
-                msg = "%8s %02d:%02d" % (k, arr.hour, arr.minute)
+                arr = datetime.fromtimestamp(v[0])
+                # msg = f'{k:9s} {arr.hour}:{arr.minute}'
+                msg = f'{k:9s} {arr.time()}, {v[0]}'
                 f.write(msg+'\n')
+                print(msg)
 
     except JSONDecodeError as e:
         print(e)
 
 
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(180)
+    main()
