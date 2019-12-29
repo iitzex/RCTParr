c779a393a72f3acaf2415311d3f5e983dfc2920f
iitzex <iitzex@gmail.com>
2019-12-29 17:03:17 +0800

    add png

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from datetime import date, datetime
 from json import JSONDecodeError
 from operator import itemgetter
 
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+
 import fr24
+
+import numpy as np
+
+register_matplotlib_converters()
 
 
 def main():
     TPE = {}
+    Tarray = []
     try:
         bounds = '71.81,-35.85,21.19,-82.17'
         j = fr24.area(bounds)
         # j = {**north, **south}
-        print(len(j))
+        # print(len(j))
 
         for k, v in j.items():
             if k == 'full_count' or k == 'version' or k == 'stats' or k == 'visible' or k == 'selected-aircraft':
@@ -34,13 +43,19 @@ def main():
                 t = {callsign: [int(ts), level, lat, lon]}
                 TPE.update(t)
                 # print(t)
+                direction = 'N' if lat > 25.08 else 'S'
+                row = {'cs': callsign,
+                       'ts': pd.to_datetime(ts, unit='s'), 'lvl': level, 'lat': lat, 'lon': lon,
+                       'direction': direction}
+                Tarray.append(row)
+
                 print('.', end='', flush=True)
         print('')
 
         with open('log', 'w') as f:
             TPE = OrderedDict(
                 sorted(TPE.items(), key=itemgetter(1), reverse=True))
-
+            # print(TPE)
             for k, v in TPE.items():
                 arr = datetime.fromtimestamp(v[0])
                 msg = ''
@@ -52,6 +67,20 @@ def main():
                     msg = f'{k:>9s}|{"":9s} {arr.time()}, {direction} :{v[1], v[2], v[3]}'
                 f.write(msg+'\n')
                 print(msg)
+
+            df = pd.DataFrame(Tarray)
+            df.sort_values(by=['ts'], inplace=True)
+            df = df[df['ts'] < df.iloc[0]['ts']+np.timedelta64(3600*4, 's')]
+            north = df[df['direction'] == 'N']
+            south = df[df['direction'] == 'S']
+
+            plt.figure(figsize=(20, 5))
+            plt.plot(df['ts'], len(df)*[0], marker='.', alpha=0.1)
+            plt.scatter(north['ts'], len(north)*[0.1], marker='v', alpha=0.33)
+            plt.scatter(south['ts'], len(south) *
+                        [-0.1], marker='^', alpha=0.33)
+            plt.gca().set_ylim([-5, 5])
+            plt.savefig('t.png')
 
     except JSONDecodeError as e:
         print(e)
